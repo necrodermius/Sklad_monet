10a1b5395cc96257cf702128db08568e43bd289f
Єлизавета Григоренко <you@example.com>
2025-04-12 16:51:32 +0300

    Added Category and Dish models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def str(self):
+        return self.name
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    image = models.ImageField(upload_to='images/', null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        verbose_name = "Dish"
+        verbose_name_plural = "Dishes"
+
+    def str(self):
+        return self.name
