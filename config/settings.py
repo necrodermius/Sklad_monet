2ec7909495e8615e007cbc07a9ed72a3050f5655
Maksym Putin <palach782@gmail.com>
2025-04-29 20:30:45 +0300

    Try static to work

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,9 +131,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# де шукати
 STATIC_URL = '/static/'
+STATICFILES_DIRS =  BASE_DIR / 'static'   # якщо маєте папку static/ у корені
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# куди collectstatic складе
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise для продакшена
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
